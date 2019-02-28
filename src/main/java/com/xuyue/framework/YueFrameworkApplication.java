d6ab10f87655c8a107f74d115477f7247e9d0b8b
xuyue <xuyue@viewhigh.com>
2019-02-28 09:57:14 +0800

    初始化工程

---
+++ src/main/java/com/xuyue/framework/YueFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.xuyue.framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YueFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YueFrameworkApplication.class, args);
+	}
+
+}
