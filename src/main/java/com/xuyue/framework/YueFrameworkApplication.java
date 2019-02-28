17e2f5dfd5b4a42c7d084f90eca9cced97d7ce4b
xuyue <xuyue@viewhigh.com>
2019-02-28 13:48:54 +0800

    aa

--- src/main/java/com/xuyue/framework/YueFrameworkApplication.java
+++ src/main/java/com/xuyue/framework/YueFrameworkApplication.java
@@ -3,6 +3,16 @@ package com.xuyue.framework;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 
+ * 类功能说明：SpringBoot启动类
+ * <p>Title:YueFrameworkApplication</p>
+ * <p>Copyright: Copyright (c) 1997~2011</p>
+ * <p>Company:北京东软望海科技有限公司</p>
+ * @author  xuyue fighterxuyue@163.com
+ * @date 2019年2月28日 下午1:34:02
+ * @version  v1.0
+ */
 @SpringBootApplication
 public class YueFrameworkApplication {
 
